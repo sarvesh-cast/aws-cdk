41c905111578d28bbe78685e07323a1a35d299f4
Aleksy Zalenski <aleksy@cast.ai>
2025-01-16 11:51:53 +0100

    Add CAST_API_KEY variable and header

--- typescript/ph2_onboarding_script/post.ts
+++ typescript/ph2_onboarding_script/post.ts
@@ -1,37 +1,39 @@
 import axios from 'axios';
 
 const sendPostRequest = async () => {
-    // Insert the role ARN created earlier
-    const ROLE_ARN = "arn:aws:iam::123456789012:role/YourRoleName";
+  // Insert the role ARN created earlier
+  const ROLE_ARN = "arn:aws:iam::123456789012:role/YourRoleName";
+  
+  // Insert the castai cluster ID
+  const CLUSTER_ID = "<<CLUSTER_ID>>"
+  const CAST_API_KEY = "<<CAST_API_KEY>>"
 
-    // Insert the castai cluster ID
-    const CLUSTER_ID = "<<CLUSTER_ID>>"
+  // API endpoint
+  const url = `https://api.cast.ai/v1/kubernetes/external-clusters/${CLUSTER_ID}`;
 
-    // API endpoint
-    const url = `https://api.cast.ai/v1/kubernetes/external-clusters/${CLUSTER_ID}`;
-
-    // Request body
-    const requestBody = {
-        eks: {
-            assumeRoleArn: ROLE_ARN
-        }
-    };
-
-    try {
-        const response = await axios.post(url, requestBody, {
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        });
-
-        console.log('Response:', response.data);
-    } catch (error) {
-        if (axios.isAxiosError(error)) {
-            console.error('Error:', error.response?.data || error.message);
-        } else {
-            console.error('Unexpected Error:', error);
-        }
+  // Request body
+  const requestBody = {
+    eks: {
+      assumeRoleArn: ROLE_ARN
     }
+  };
+
+  try {
+    const response = await axios.post(url, requestBody, {
+      headers: {
+        'Content-Type': 'application/json',
+        'X-API-Key': CAST_API_KEY,
+      },
+    });
+
+    console.log('Response:', response.data);
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.error('Error:', error.response?.data || error.message);
+    } else {
+      console.error('Unexpected Error:', error);
+    }
+  }
 };
 
 sendPostRequest();
